531359d59bd4e3ec4809e8fe3a079e3223786199
dinhhiep110 <dinhhiep110@gmail.com>
2023-02-09 18:39:43 +0700

    Add project files.

---
+++ DemoPRN/MyDBContext.cs
@@ -0,0 +1,25 @@
+﻿using DemoPRN.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class MyDbContext : DbContext
+{
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+           : base(options)
+    {
+    }
+ 
+
+    public virtual DbSet<Company> Companies { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder optionsBuilder)
+    {
+        optionsBuilder.Entity<Company>().HasData(
+            new Company { Id = Guid.NewGuid(), Name = "FPT SoftWare" , Address = "Ha Noi" ,Country = "Viet Nam"},
+            new Company { Id = Guid.NewGuid(), Name = "NTQ" , Address = "Ha Noi" ,Country = "Viet Nam"},
+            new Company { Id = Guid.NewGuid(), Name = "Base Vn" , Address = "Ha Noi" ,Country = "Viet Nam"},
+            new Company { Id = Guid.NewGuid(), Name = "Nash Tech" , Address = "Ha Noi" ,Country = "Viet Nam"}
+        );
+    }
+}
