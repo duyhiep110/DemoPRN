72039066852f4025141b79c81c8f69cae6e07638
Quang Khuat Tien <quang.khuat@ntq-solution.com.vn>
2023-02-12 12:59:46 +0700

    feature-employee

---
+++ DemoPRN/Controllers/EmployeeController.cs
@@ -0,0 +1,94 @@
+﻿using AutoMapper;
+using DemoPRN.Dtos.Company;
+using DemoPRN.Dtos.Employee;
+using DemoPRN.Repository;
+using DemoPRN.Repository.Implement;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoPRN.Controllers
+{
+    [Route("api/companies/{companyId}/employees")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly IMapper _mapper;
+
+        public EmployeeController(IRepositoryManager repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetEmployeesForCompany(Guid companyId) {
+            var company = _repository.CompanyRepository.GetCompany(companyId, trackChanges: false);
+            if(company == null)
+            {
+                return NotFound(); 
+            }
+            var employeesFromDb = _repository.EmployeeRepository.GetEmployees(companyId, trackChanges: false);
+            var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
+            return Ok(employeesDto);
+        }
+
+        [HttpGet("{id}", Name = " GetEmployeeForCompany")]
+        public IActionResult GetEmployeesForCompany(Guid companyId,Guid id) {
+            var company = _repository.CompanyRepository.GetCompany(companyId,trackChanges: false);
+            if(company == null)
+            {
+                return NotFound();
+            }
+            var employeeDb = _repository.EmployeeRepository.GetEmployee(companyId, trackChanges: false);
+            if(employeeDb == null)
+            {
+                return NotFound();
+            }
+            var employee = _mapper.Map<EmployeeDto>(employeeDb);
+            return Ok(employee);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployeeForCompany(Guid companyId, Guid id) {
+            var company = _repository.CompanyRepository.GetCompany(companyId, trackChanges: false);
+            if(company!= null)
+            {
+                return NotFound();
+            }
+            var employeeForCompany = _repository.EmployeeRepository.GetEmployee(companyId,id,trackChanges: false);
+            if(employeeForCompany == null) { 
+                return NotFound();
+            }
+            _repository.EmployeeRepository.DeleteEmployee(employeeForCompany);
+            _repository.Save();
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateEmployeeForCompany(Guid companyId, Guid id, [FromBody] EmployeeForUpdateDto dto)
+        {
+            if (dto == null)
+            {
+                return BadRequest("EmployeeForUpdateDto object is null");
+            }
+            if (!ModelState.IsValid)
+            {
+                return UnprocessableEntity(ModelState);
+            }
+            var company = _repository.CompanyRepository.GetCompany(id, false);
+            if (company == null)
+            {
+                return NotFound("No Company is founded");
+            }
+            var employeeeEntity = _repository.EmployeeRepository.GetEmployee(companyId, id, true);
+            if (employeeeEntity == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(dto, employeeeEntity);
+            _repository.Save();
+            return NoContent();
+        }
+    }
+}
+
---
+++ DemoPRN/Migrations/20230212050100_IntialDB.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoPRN.Migrations
+{
+    public partial class IntialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "CompanyId",
+                keyValue: new Guid("04e68dd3-f57c-45ab-b821-9186fe634e82"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "CompanyId",
+                keyValue: new Guid("22f2d665-2618-438c-a55b-f557b7a0df0c"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "CompanyId",
+                keyValue: new Guid("9f0dd7dc-3005-4af9-937e-5b194d40948c"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "CompanyId",
+                keyValue: new Guid("da44f382-ea48-4af1-8cf2-577d0f797267"));
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "CompanyId", "Address", "Country", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("8bb4589f-9702-400d-836f-91d27522c3ab"), "Ha Noi", "Viet Nam", "Base Vn" },
+                    { new Guid("9d80dfec-feb2-4c8e-8bdc-4422a7cef476"), "Ha Noi", "Viet Nam", "FPT SoftWare" },
+                    { new Guid("ca9d6a4a-f5fa-4609-9ea1-8d37286eab07"), "Ha Noi", "Viet Nam", "NTQ" },
+                    { new Guid("f149995b-7f54-42c6-807d-b43990de5ff4"), "Ha Noi", "Viet Nam", "Nash Tech" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "CompanyId",
+                keyValue: new Guid("8bb4589f-9702-400d-836f-91d27522c3ab"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "CompanyId",
+                keyValue: new Guid("9d80dfec-feb2-4c8e-8bdc-4422a7cef476"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "CompanyId",
+                keyValue: new Guid("ca9d6a4a-f5fa-4609-9ea1-8d37286eab07"));
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "CompanyId",
+                keyValue: new Guid("f149995b-7f54-42c6-807d-b43990de5ff4"));
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "CompanyId", "Address", "Country", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("04e68dd3-f57c-45ab-b821-9186fe634e82"), "Ha Noi", "Viet Nam", "FPT SoftWare" },
+                    { new Guid("22f2d665-2618-438c-a55b-f557b7a0df0c"), "Ha Noi", "Viet Nam", "NTQ" },
+                    { new Guid("9f0dd7dc-3005-4af9-937e-5b194d40948c"), "Ha Noi", "Viet Nam", "Base Vn" },
+                    { new Guid("da44f382-ea48-4af1-8cf2-577d0f797267"), "Ha Noi", "Viet Nam", "Nash Tech" }
+                });
+        }
+    }
+}
